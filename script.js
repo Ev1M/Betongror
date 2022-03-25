337110deae7e0826d319ada370217bf611b9a1f1
Ev1M <90681485+Ev1M@users.noreply.github.com>
2022-03-25 11:43:55 +0100

    fiksni

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ var Sekk = 12500;
 function Utregning() {
     var ytreRadius = YtreEl.value /2;
     var ytreGrunnflate = Math.PI*(ytreRadius*ytreRadius);
-    var ytreVolum = Math.round(ytreGrunnflate*LengdeEl.value);
+    var ytreVolum = parseInt(ytreGrunnflate*LengdeEl.value);
     console.log(ytreVolum + "cm^2")
 
     var indreRadius = IndreEl.value /2;
@@ -19,7 +19,8 @@ function Utregning() {
     console.log(betongVolum + "cm^3")
 
     var forhold = betongVolum / Sekk;
-    var pris = forhold * 89;
-    console.log(parseInt(pris) + "kr")
+    var pris = parseInt(forhold) * 89;
+    
+    document.write("pris " + parseInt(pris) + "kr  volum " + parseInt(betongVolum) + "cm^3 og " + parseInt(forhold) + " antall sekker");
 
 };
